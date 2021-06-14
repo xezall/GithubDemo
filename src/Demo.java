cb4d912048b986357f472ebf57b491050a498bda
xezall <ceylanxezalkaya@gmail.com>
2021-06-14 19:42:42 -0400

    this is created by me for understand how i can use guthub:)

--- src/Demo.java
+++ src/Demo.java
@@ -1,2 +1,2 @@
-public class Demo {
+public cla
 }
---
+++ src/Demo/demo.java
@@ -0,0 +1,9 @@
+package Demo;
+
+
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("i am happy and thankful");
+    }
+}
